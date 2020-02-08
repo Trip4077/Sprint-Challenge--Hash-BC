d50529d19a396c39109cf1446d14c41e5393a2dd
Trip4077 <bj2336@email.vccs.edu>
2020-02-08 12:53:29 -0500

    ex2 complete

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -19,5 +19,23 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
+    # Store all the Tickets in a hashtable using source as key
+    for i in range( length ):
+        hash_table_insert( hashtable, tickets[i].source, tickets[i] )
 
+    # Get the starting Ticket for route
+    current_ticket = hash_table_retrieve( hashtable, 'NONE' )
+    #Numer of Tickets inserted in Route
+    count = 0
+
+    # Add the current ticket's destination to route list and move to next ticket
+    while current_ticket.destination != "NONE":
+        route[count] = current_ticket.destination
+        count += 1
+
+        current_ticket = hash_table_retrieve( hashtable, current_ticket.destination )
+    
+    # Add final destination to list
+    route[count] = current_ticket.destination
+   
     return route
